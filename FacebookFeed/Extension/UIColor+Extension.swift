d5419f65f7c1842d8872d8bd30db90e010cbab64
Hai Tran <tuanhai.dev@gmail.com>
2018-03-04 00:18:50 +0700

    Fill collection view with static data

--- FacebookFeed/Extension/UIColor+Extension.swift
+++ FacebookFeed/Extension/UIColor+Extension.swift
@@ -7,4 +7,5 @@ extension UIColor {
                                               alpha: 1)
   static let navigationBarTitleTextColor = UIColor.white
   static let collectionViewBackgroundColor = UIColor(white: 0.95, alpha: 1)
+  static let secondLineAttributedTextColor = UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)
 }
--- FacebookFeed/FeedCollectionViewCell.swift
+++ FacebookFeed/FeedCollectionViewCell.swift
@@ -4,17 +4,39 @@ class FeedCollectionViewCell: UICollectionViewCell {
   // MARK: - Properties
   let nameLabel: UILabel = {
     let label = UILabel()
-    label.text = "Sample"
-    label.backgroundColor = UIColor.gray
-    label.font = UIFont.boldSystemFont(ofSize: 14)
+    label.numberOfLines = 2
+    
+    
+    label.attributedText = labelText
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
+  }()
+  
+  private static let labelText: NSAttributedString = {
+    let attributedString = NSMutableAttributedString(string: "Mark Zuckerberg",
+                                                     attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+    let secondLine = NSAttributedString(string: "\nDecember 18 - Sanfrancisco   ",
+                                        attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12),
+                                                     NSAttributedString.Key.foregroundColor: UIColor.secondLineAttributedTextColor
+      ])
+    attributedString.append(secondLine)
+    
+    let paragraphStyle = NSMutableParagraphStyle()
+    paragraphStyle.lineSpacing = 4
+    attributedString.addAttributes([NSAttributedString.Key.paragraphStyle: paragraphStyle], range: NSMakeRange(0, attributedString.string.count))
+    
+    let attachment = NSTextAttachment()
+    attachment.image = UIImage(named: "globe_small")
+    attachment.bounds = CGRect(x: 0, y: -2, width: 12, height: 12)
+    attributedString.append(NSAttributedString(attachment: attachment))
+    
+    return attributedString
   }()
   
   let profileImageView: UIImageView = {
     let imageView = UIImageView()
     imageView.contentMode = .scaleAspectFit
-    imageView.backgroundColor = UIColor.red
+    imageView.image = UIImage(named: "zuckprofile")
     imageView.translatesAutoresizingMaskIntoConstraints = false
     return imageView
   }()
