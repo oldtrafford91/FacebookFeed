95cfe4c114f1864c582602cdeb1fbd2a73637cd8
Hai Tran <tuanhai.dev@gmail.com>
2018-03-05 16:36:23 +0700

    Change color constant using new UIColor extension function

--- FacebookFeed/Extension/UIColor+Extension.swift
+++ FacebookFeed/Extension/UIColor+Extension.swift
@@ -1,15 +1,12 @@
 import UIKit
 
 extension UIColor {
-  static let navigationBarTintColor = UIColor(red: 66/255,
-                                              green: 103/255,
-                                              blue: 178/255,
-                                              alpha: 1)
+  static let navigationBarTintColor = UIColor.rgb(red: 66, green: 103, blue: 178)
   static let navigationBarTitleTextColor = UIColor.white
   static let collectionViewBackgroundColor = UIColor(white: 0.95, alpha: 1)
-  static let secondLineAttributedTextColor = UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)
+  static let secondLineAttributedTextColor = UIColor.rgb(red: 155, green: 161, blue: 171)
   
   static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
-    return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
   }
 }
