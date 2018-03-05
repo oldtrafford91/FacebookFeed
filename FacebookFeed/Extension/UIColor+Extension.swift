031beb71b11bd36f2d9ccf0517b9d209b9d17a54
Hai Tran <tuanhai.dev@gmail.com>
2018-03-05 16:33:13 +0700

    Add convenient function to create color from RGB

--- FacebookFeed/Extension/UIColor+Extension.swift
+++ FacebookFeed/Extension/UIColor+Extension.swift
@@ -8,4 +8,8 @@ extension UIColor {
   static let navigationBarTitleTextColor = UIColor.white
   static let collectionViewBackgroundColor = UIColor(white: 0.95, alpha: 1)
   static let secondLineAttributedTextColor = UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)
+  
+  static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+    return UIColor(red: red, green: green, blue: blue, alpha: 1)
+  }
 }
