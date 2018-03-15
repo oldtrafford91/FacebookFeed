4461acf0518e9cd3585929ecaf776ad8ad02a89f
Hai Tran <tuanhai.dev@gmail.com>
2018-03-16 00:48:46 +0700

    Finishing layout with static content

--- FacebookFeed/FeedCollectionViewCell.swift
+++ FacebookFeed/FeedCollectionViewCell.swift
@@ -48,10 +48,62 @@ class FeedCollectionViewCell: UICollectionViewCell {
     let imageView = UIImageView()
     imageView.contentMode = .scaleAspectFill
     imageView.image = UIImage(named: "zuckdog")
-    imageView.clipsToBounds = true
+    imageView.layer.masksToBounds = true
     return imageView
   }()
   
+  let likesCommentsLabel: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 12)
+    label.text = "400 Likes   10.7K Comments"
+    label.textColor = UIColor.rgb(red: 155, green: 161, blue: 171)
+    return label
+  }()
+  
+  let separator: UIView = {
+    let view = UIView()
+    view.backgroundColor = UIColor.rgb(red: 226, green: 228, blue: 232)
+    return view
+  }()
+  
+  let buttonStackView: UIStackView = {
+    let stackview = UIStackView()
+    stackview.alignment = .center
+    stackview.axis = .horizontal
+    stackview.distribution = .equalSpacing
+    return stackview
+  }()
+  
+  let likeButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Like", for: .normal)
+    button.setTitleColor(UIColor.rgb(red: 143, green: 150, blue: 163), for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+    button.changeButtonTitleAndImageSpacing(spacing: 8)
+    button.setImage(UIImage(named: "like"), for: .normal)
+    return button
+  }()
+  
+  let commentButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Comment", for: .normal)
+    button.setTitleColor(UIColor.rgb(red: 143, green: 150, blue: 163), for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+    button.changeButtonTitleAndImageSpacing(spacing: 8)
+    button.setImage(UIImage(named: "comment"), for: .normal)
+    return button
+  }()
+  
+  let shareButton: UIButton = {
+    let button = UIButton()
+    button.setTitle("Share", for: .normal)
+    button.setTitleColor(UIColor.rgb(red: 143, green: 150, blue: 163), for: .normal)
+    button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+    button.changeButtonTitleAndImageSpacing(spacing: 8)
+    button.setImage(UIImage(named: "share"), for: .normal)
+
+    return button
+  }()
   // MARK: - Init
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -64,14 +116,24 @@ class FeedCollectionViewCell: UICollectionViewCell {
   // MARK: - Setup
   func setupViews() {
     backgroundColor = UIColor.white
-    addSubview(nameLabel)
-    addSubview(profileImageView)
-    addSubview(statusTextView)
-    addSubview(statusImageView)
+    for button in [likeButton, commentButton, shareButton] {
+      buttonStackView.addArrangedSubview(button)
+    }
+    
+    for view in [nameLabel, profileImageView,
+                 statusTextView, statusImageView,
+                 likesCommentsLabel, separator,
+                 buttonStackView] {
+                  addSubview(view)
+    }
+    
     addConstraintWithVisualFormat(format: "H:|-8-[v0(44)]-8-[v1]|", views: profileImageView, nameLabel)
-    addConstraintWithVisualFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]-4-[v2]|", views: profileImageView, statusTextView, statusImageView)
     addConstraintWithVisualFormat(format: "V:|-12-[v0]", views: nameLabel)
     addConstraintWithVisualFormat(format: "H:|-4-[v0]-4-|", views: statusTextView)
     addConstraintWithVisualFormat(format: "H:|[v0]|", views: statusImageView)
+    addConstraintWithVisualFormat(format: "H:|-12-[v0]|", views: likesCommentsLabel)
+    addConstraintWithVisualFormat(format: "H:|-12-[v0]-12-|", views: separator)
+    addConstraintWithVisualFormat(format: "H:|-12-[v0]-12-|", views: buttonStackView)
+    addConstraintWithVisualFormat(format: "V:|-8-[v0(44)]-4-[v1(30)]-4-[v2]-8-[v3(24)]-8-[v4(0.4)]-8-[v5(44)]-8-|", views: profileImageView, statusTextView, statusImageView, likesCommentsLabel, separator, buttonStackView)
   }
 }
